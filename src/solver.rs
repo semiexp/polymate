9bb9ec2d00c54de72dac0a2b1d1cc3fa242a1e4f
semiexp <linthya.v@gmail.com>
2018-02-11 15:12:57 +0900

    Save answer_raw and improve performance in search

--- src/solver.rs
+++ src/solver.rs
@@ -5,13 +5,14 @@ pub fn solve(problem: &Puzzle) -> u64 {
     let dic = Dictionary::new(problem);
 
     let mut rem_piece = dic.piece_count.clone();
+    let mut answer_raw = vec![(-1, -1); dic.n_target_cells as usize];
 
-    search(&dic, &mut rem_piece, 0u64)
+    search(&dic, &mut rem_piece, &mut answer_raw, 0u64)
 }
 
-fn search(dic: &Dictionary, rem_piece: &mut Vec<i32>, mask: u64) -> u64 {
+fn search(dic: &Dictionary, rem_piece: &mut Vec<i32>, answer_raw: &mut Vec<(i32, i32)>, mask: u64) -> u64 {
     let pos = (!mask).trailing_zeros() as i32;
-    
+
     if pos == dic.n_target_cells {
         return 1u64;
     }
@@ -19,16 +20,20 @@ fn search(dic: &Dictionary, rem_piece: &mut Vec<i32>, mask: u64) -> u64 {
     let mut ret = 0u64;
 
     for i in 0..rem_piece.len() {
-        if rem_piece[i] > 0 {
-            rem_piece[i] -= 1;
-            for m in &dic.placements[pos as usize][i] {
+        if unsafe { *rem_piece.get_unchecked(i) } > 0 {
+            unsafe { *rem_piece.get_unchecked_mut(i) -= 1 };
+            let pl = unsafe { dic.placements.get_unchecked(pos as usize).get_unchecked(i) };
+            for j in 0..pl.len() {
+                let m = unsafe { *pl.get_unchecked(j) };
                 if (mask & m) == 0 {
-                    ret += search(dic, rem_piece, mask | m);
+                    unsafe { *answer_raw.get_unchecked_mut(pos as usize) = (i as i32, j as i32); }
+                    ret += search(dic, rem_piece, answer_raw, mask | m);
                 }
             }
-            rem_piece[i] += 1;
+            unsafe { *rem_piece.get_unchecked_mut(i) += 1 };
         }
     }
 
+    unsafe { *answer_raw.get_unchecked_mut(pos as usize) = (-1, -1); }
     ret
 }
