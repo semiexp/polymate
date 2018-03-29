a7e82cc5e71f071dfd3ff04077d711bd8e31df57
semiexp <linthya.v@gmail.com>
2018-03-29 13:42:34 +0900

    Use u64 for rem_piece in solver

--- src/solver.rs
+++ src/solver.rs
@@ -3,52 +3,134 @@ use super::*;
 // just counting # of answers
 pub fn solve(problem: &Puzzle) -> Answers {
     let use_bitset = (problem.target.volume() > 64);
-    let mut answers;
 
     if use_bitset {
-        let mut dic = Dictionary::<Bitset>::new(problem);
-
-        let mut answer_raw = vec![(-1, -1); dic.n_target_cells as usize];
-        answers = Answers::new();
-        
-        for i in 0..dic.initial_piece_count.len() {
-            let mut rem_piece = dic.initial_piece_count[i].clone();
-            dic.target_symmetry = dic.initial_symmetry[i];
-
-            for &(cell, piece, ori) in &dic.initial_placement_id[i] {
-                answer_raw[cell as usize] = (piece, ori);
-            }
-            
-            let mut pl = dic.initial_placement[i].clone();
-            search_generic(&dic, &mut rem_piece, &mut answer_raw, &mut pl, &mut answers);
-
-            for &(cell, piece, ori) in &dic.initial_placement_id[i] {
-                answer_raw[cell as usize] = (-1, -1);
-            }
-        }
+        solve_typed::<Bitset>(problem)
     } else {
-        let mut dic = Dictionary::<u64>::new(problem);
+        solve_typed::<u64>(problem)
+    }
+}
 
-        let mut answer_raw = vec![(-1, -1); dic.n_target_cells as usize];
-        answers = Answers::new();
+fn solve_typed<T: Bits + SearchHandler>(problem: &Puzzle) -> Answers {
+    let mut dic = Dictionary::<T>::new(problem);
+
+    let mut answer_raw = vec![(-1, -1); dic.n_target_cells as usize];
+    let mut answers = Answers::new();
+    
+    for i in 0..dic.initial_piece_count.len() {
+        let mut rem_piece = dic.initial_piece_count[i].clone();
+        dic.target_symmetry = dic.initial_symmetry[i];
+
+        for &(cell, piece, ori) in &dic.initial_placement_id[i] {
+            answer_raw[cell as usize] = (piece, ori);
+        }
         
-        for i in 0..dic.initial_piece_count.len() {
-            let mut rem_piece = dic.initial_piece_count[i].clone();
-            dic.target_symmetry = dic.initial_symmetry[i];
+        T::search(&dic, &mut rem_piece, &mut answer_raw, dic.initial_placement[i].clone(), &mut answers);
 
-            for &(cell, piece, ori) in &dic.initial_placement_id[i] {
-                answer_raw[cell as usize] = (piece, ori);
-            }
-            
-            search(&dic, &mut rem_piece, &mut answer_raw, dic.initial_placement[i], &mut answers);
-
-            for &(cell, piece, ori) in &dic.initial_placement_id[i] {
-                answer_raw[cell as usize] = (-1, -1);
-            }
+        for &(cell, piece, ori) in &dic.initial_placement_id[i] {
+            answer_raw[cell as usize] = (-1, -1);
         }
     }
 
     answers
+}
+
+trait SearchHandler : Bits {
+    fn search(dic: &Dictionary<Self>, rem_piece: &mut Vec<i32>, answer_raw: &mut Vec<(i32, i32)>, mask_default: Self, answers: &mut Answers);
+}
+
+impl SearchHandler for u64 {
+    fn search(dic: &Dictionary<Self>, rem_piece: &mut Vec<i32>, answer_raw: &mut Vec<(i32, i32)>, mask_default: Self, answers: &mut Answers) {
+        let mut is_zero_one = true;
+        for i in 0..rem_piece.len() {
+            if rem_piece[i] > 1 {
+                is_zero_one = false;
+                break;
+            }
+        }
+
+        if is_zero_one && rem_piece.len() <= 64 {
+            let mut rem_piece_bits = 0u64;
+            for i in 0..rem_piece.len() {
+                rem_piece_bits |= (rem_piece[i] as u64) << (i as u64);
+            }
+            search_with_u64_rem_piece(dic, rem_piece_bits, answer_raw, mask_default, answers);
+        } else {
+            search(dic, rem_piece, answer_raw, mask_default, answers);
+        }
+    }
+}
+
+impl SearchHandler for Bitset {
+    fn search(dic: &Dictionary<Self>, rem_piece: &mut Vec<i32>, answer_raw: &mut Vec<(i32, i32)>, mask_default: Self, answers: &mut Answers) {
+        let mut mask_default = mask_default;
+        search_generic(dic, rem_piece, answer_raw, &mut mask_default, answers);
+    }
+}
+
+#[derive(Clone, Copy)]
+struct PieceCountBitSeq {
+    val: u64
+}
+
+struct PieceCountBitSeqIter {
+    val: u64
+}
+
+impl IntoIterator for PieceCountBitSeq {
+    type Item = usize;
+    type IntoIter = PieceCountBitSeqIter;
+
+    fn into_iter(self) -> PieceCountBitSeqIter {
+        PieceCountBitSeqIter { val: self.val }
+    }
+}
+
+impl Iterator for PieceCountBitSeqIter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<usize> {
+        if self.val != 0 {
+            let ret = self.val.trailing_zeros();
+            self.val ^= 1u64 << (ret as u64);
+            Some(ret as usize)
+        } else {
+            None
+        }
+    }
+}
+
+fn search_with_u64_rem_piece(dic: &Dictionary<u64>, rem_piece: u64, answer_raw: &mut Vec<(i32, i32)>, mask: u64, answers: &mut Answers) {
+    let pos = (!mask).trailing_zeros() as i32;
+
+    if pos == dic.n_target_cells {
+        let mut rem_piece_as_vec = vec![0i32; dic.piece_count.len()];
+        for i in 0..rem_piece_as_vec.len() {
+            rem_piece_as_vec[i] = ((rem_piece >> i) & 1) as i32;
+        }
+        save_answer(dic, &mut rem_piece_as_vec, answer_raw, answers);
+        return;
+    }
+
+    if isolated_cell_pruning(dic, mask) { return; }
+
+    let rem_piece_orig = rem_piece;
+    let mut rem_piece = rem_piece;
+    while rem_piece != 0 {
+        let i = rem_piece.trailing_zeros() as usize;
+        rem_piece ^= 1u64 << (i as u64);
+        let pl = unsafe { dic.placements.get_unchecked(pos as usize).get_unchecked(i) };
+        for j in 0..pl.len() {
+            let m = unsafe { *pl.get_unchecked(j) };
+            answers.search_steps += 1;
+            if (mask & m) == 0 {
+                unsafe { *answer_raw.get_unchecked_mut(pos as usize) = (i as i32, j as i32); }
+                search_with_u64_rem_piece(dic, rem_piece_orig ^ (1u64 << (i as u64)), answer_raw, mask | m, answers);
+            }
+        }
+    }
+
+    unsafe { *answer_raw.get_unchecked_mut(pos as usize) = (-1, -1); }
 }
 
 fn search(dic: &Dictionary<u64>, rem_piece: &mut Vec<i32>, answer_raw: &mut Vec<(i32, i32)>, mask: u64, answers: &mut Answers) {
@@ -59,18 +141,8 @@ fn search(dic: &Dictionary<u64>, rem_piece: &mut Vec<i32>, answer_raw: &mut Vec<
         return;
     }
 
-    if dic.isolated_cell_pruning {
-        if (!mask
-         & ((mask << dic.isolated_cell_pruning_x_ofs) | dic.isolated_cell_pruning_x_mask_lo)
-         & ((mask >> dic.isolated_cell_pruning_x_ofs) | dic.isolated_cell_pruning_x_mask_hi)
-         & ((mask << dic.isolated_cell_pruning_y_ofs) | dic.isolated_cell_pruning_y_mask_lo)
-         & ((mask >> dic.isolated_cell_pruning_y_ofs) | dic.isolated_cell_pruning_y_mask_hi)
-         & ((mask << dic.isolated_cell_pruning_z_ofs) | dic.isolated_cell_pruning_z_mask_lo)
-         & ((mask >> dic.isolated_cell_pruning_z_ofs) | dic.isolated_cell_pruning_z_mask_hi)
-        ) != 0 {
-            return;
-        }
-    }
+    if isolated_cell_pruning(dic, mask) { return; }
+
     for i in 0..rem_piece.len() {
         if unsafe { *rem_piece.get_unchecked(i) } > 0 {
             unsafe { *rem_piece.get_unchecked_mut(i) -= 1 };
@@ -88,6 +160,18 @@ fn search(dic: &Dictionary<u64>, rem_piece: &mut Vec<i32>, answer_raw: &mut Vec<
     }
 
     unsafe { *answer_raw.get_unchecked_mut(pos as usize) = (-1, -1); }
+}
+
+fn isolated_cell_pruning(dic: &Dictionary<u64>, mask: u64) -> bool {
+    dic.isolated_cell_pruning &&
+           (!mask
+         & ((mask << dic.isolated_cell_pruning_x_ofs) | dic.isolated_cell_pruning_x_mask_lo)
+         & ((mask >> dic.isolated_cell_pruning_x_ofs) | dic.isolated_cell_pruning_x_mask_hi)
+         & ((mask << dic.isolated_cell_pruning_y_ofs) | dic.isolated_cell_pruning_y_mask_lo)
+         & ((mask >> dic.isolated_cell_pruning_y_ofs) | dic.isolated_cell_pruning_y_mask_hi)
+         & ((mask << dic.isolated_cell_pruning_z_ofs) | dic.isolated_cell_pruning_z_mask_lo)
+         & ((mask >> dic.isolated_cell_pruning_z_ofs) | dic.isolated_cell_pruning_z_mask_hi)
+        ) != 0
 }
 
 fn search_generic<T: Bits>(dic: &Dictionary<T>, rem_piece: &mut Vec<i32>, answer_raw: &mut Vec<(i32, i32)>, mask: &mut T, answers: &mut Answers) {
